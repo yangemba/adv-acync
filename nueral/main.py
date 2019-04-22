5006621fc7f64788a8e7e767e02adda70cdd1ceb
yan.hemba <yan.hemba@toweya.net>
2019-04-22 16:48:28 +0300

    [add]nureral

---
+++ nueral/main.py
@@ -0,0 +1,25 @@
+
+
+
+YES = 'yes'
+NO = 'no'
+
+
+data_set = {'lolololololo': YES, 'koko':NO, 'sffds': NO, 'loiiiiiiiiiiiiirrrrrrrrrrrrrrrrp': YES }
+
+
+class Network:
+    def __init__(self):
+
+
+        self.YES_PC = int
+        self.NO_PC = int
+
+    def actualize(self, data_dict):
+       for element in data_dict:
+
+
+
+
+
+
